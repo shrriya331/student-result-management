c1ceadb652a7792be4699bc2e9f23424fc07e91d
Shriya P <shriyaaa331@gmail.com>
2025-06-15 11:40:52 +0530

    Registered models in admin and created superuser

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student, Subject, Marks
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(Marks)
+
